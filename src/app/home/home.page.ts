720bc3236a5e4e7649d01daf5de24d2ee8714e6a
CodyNetsh <khodani1994@gmail.com>
2019-07-17 15:24:07 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,12 +9,70 @@ import { Router } from '@angular/router';
 export class HomePage {
 TA=0;
 TB=0;
+num:number;
   constructor(private router:Router) {}
 
+  press1() {
+    console.log(this.TA -= 3)
+   }
+
+  press2() {
+   console.log(this.TA += 3)
+  }
+  press3() {
+    console.log(this.TA -= 2)
+   }
+
+  press4() {
+   console.log(this.TA += 2)
+  }
+  press5() {
+    console.log(this.TA -= 1)
+   }
+
+  press6() {
+   console.log(this.TA += 1)
+  }
+  press7() {
+    console.log(this.TA -= 3)
+   }
+
+  press8() {
+   console.log(this.TB += 3)
+  }
+  press9() {
+    console.log(this.TB -= 2)
+   }
+
+  press10() {
+   console.log(this.TB += 2)
+  }
+  press11() {
+    console.log(this.TB -= 1)
+   }
+
+  press12() {
+   console.log(this.TB += 1)
+  }
   
+  press13() {
+    console.log(this.TA -= 4)
+   }
+
+  press14() {
+   console.log(this.TA += 4)
+  }
+  press15() {
+    console.log(this.TB -= 4)
+   }
+
+  press16() {
+   console.log(this.TB += 4)
+  }
+
   reset() {
-    console.log(this.TA) 
-    console.log(this.TB) 
+    console.log(this.TA =0) 
+    console.log(this.TB =0) 
     this.router.navigateByUrl('/home')
   }
 }
